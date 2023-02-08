4be47be0f338f8ac99e7c6c4fa5d50ef6af458a4
veronicavoevodina <veranika1711@gmail.com>
2023-02-08 13:17:23 +0300

    feat: create project

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,15 @@
 import React from 'react';
 
+import img from '../images/about.jpeg';
+import Title from './Title';
 const About = () => {
   return (
     <section className="section" id="about">
-      <div className="section-title">
-        <h2>
-          about <span>us</span>
-        </h2>
-      </div>
+      <Title title="about" subTitle="us" />
 
       <div className="section-center about-center">
         <div className="about-img">
-          <img
-            src="./images/about.jpeg"
-            className="about-photo"
-            alt="awesome beach"
-          />
+          <img src={img} className="about-photo" alt="awesome beach" />
         </div>
         <article className="about-info">
           <h3>explore the difference</h3>
@@ -29,7 +23,7 @@ const About = () => {
             quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
             unde dolor?
           </p>
-          <a href="#" className="btn">
+          <a href="#home" className="btn">
             read more
           </a>
         </article>
