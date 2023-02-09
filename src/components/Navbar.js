cfe8d692d0fbb8db48e1527bf9cce1bb4def4f0b
veronicavoevodina <veranika1711@gmail.com>
2023-02-09 12:27:37 +0300

    feat: add burger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import img from '../images/logo.svg';
 import { DataList } from '../Data';
 import { socialLinks } from '../Data';
 
 const Navbar = () => {
+  const [active, setActive] = useState(false);
+  useEffect(() => {
+    if (active) {
+      document.addEventListener('click', function (e) {
+        if (
+          e.target.className !== 'navbar-link' &&
+          e.target.className !== 'fas fa-bars'
+        ) {
+          setActive(false);
+        }
+      });
+    }
+  }, [active]);
   return (
     <nav className="navbar">
       <div className="nav-center">
         <div className="nav-header">
           <img src={img} className="nav-logo" alt="backroads" />
-          <button type="button" className="nav-toggle" id="nav-toggle">
+          <ul className={active ? 'navbar-links active' : 'navbar-links'}>
+            {DataList.map((link) => {
+              return (
+                <li
+                  key={link.id}
+                  style={{ width: '100%', textAlign: 'center' }}
+                  onClick={() => setActive(false)}
+                >
+                  <a href={link.href} className="navbar-link">
+                    {link.text}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+
+          <button
+            type="button"
+            className="nav-toggle"
+            id="nav-toggle"
+            onClick={() => setActive(!active)}
+          >
             <i className="fas fa-bars"></i>
           </button>
         </div>
